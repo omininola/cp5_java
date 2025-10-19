2f7fccbc9ff572cb7d97df62b2f34be473d75a2c
Sofia Andrade Petruk <157611475+sofiapetruk@users.noreply.github.com>
2025-10-19 19:50:07 -0300

    Delete parte2 directory

--- parte2/src/main/java/br/com/fiap/cp5/cp5_java/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.cp5.cp5_java.repository;
-
-import br.com.fiap.cp5.cp5_java.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
